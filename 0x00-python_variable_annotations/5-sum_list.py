1359df51ead156b66977565f1b99a0be379f15d0
itsmraga-hub <ragawilliam570@gmail.com>
2023-08-03 17:01:59 +0300

    Task 5: return sum_list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Write a type-annotated function sum_list which takes a list
+    input_list of floats as argument and returns their sum as a float.
+"""
+import functools
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+        Find sum of input list of float numbers
+    """
+    return functools.reduce(lambda a, b: a + b, input_list)
