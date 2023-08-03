ef57bf0793cf1a5f91a37eaefa6578515386ce15
itsmraga-hub <ragawilliam570@gmail.com>
2023-08-03 17:00:46 +0300

    Task 1: concat function - typed

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Write a type-annotated function concat that takes a string str1
+    and a string str2 as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        concat takes two strings and joins them together
+    """
+    return str1 + str2
