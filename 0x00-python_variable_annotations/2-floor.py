e2d0a9ca1c3fcd25bc5da5cc3f647364802b0773
itsmraga-hub <ragawilliam570@gmail.com>
2023-08-03 17:01:07 +0300

    Task 2: return floor of a float number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Write a type-annotated function floor which takes a float n
+    as argument and returns the floor of the float.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+        floor behaves like a math.floor function, returning
+        an int number that's rounded down.
+    """
+    return math.floor(n)
