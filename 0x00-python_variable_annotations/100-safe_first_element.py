aa4723c7648b8939eee381cc044e4ce3cfcf4a92
itsmraga-hub <ragawilliam570@gmail.com>
2023-08-03 20:57:29 +0300

    Task 10: augment code with correct annotations

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+    Augment the following code with the correct duck-typed annotations:
+
+    # The types of the elements of the input are not know
+    def safe_first_element(lst):
+        if lst:
+            return lst[0]
+        else:
+            return None
+"""
+from typing import Sequence, Any, Union, Any
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+        Augment the following code with the correct duck-typed annotations:
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
