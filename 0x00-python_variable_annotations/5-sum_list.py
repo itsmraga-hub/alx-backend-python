bd6d10de2751a4c699f625528ce3f6e6305dbe5e
itsmraga-hub <ragawilliam570@gmail.com>
2023-08-03 20:27:13 +0300

    Update List types

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,10 +3,11 @@
     Write a type-annotated function sum_list which takes a list
     input_list of floats as argument and returns their sum as a float.
 """
+from typing import List
 import functools
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
         Find sum of input list of float numbers
     """
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,9 +4,10 @@
     mxd_lst of integers and floats and returns their sum as a float.
 """
 import functools
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: list(int|float)): -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]): -> float:
     """
         A function that adds both ints and floats
         and returns a float
