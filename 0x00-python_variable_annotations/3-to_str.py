a6e34f5a8782d083161e4659a584fc370029d565
itsmraga-hub <ragawilliam570@gmail.com>
2023-08-03 17:01:25 +0300

    Task 3: Convrt from float/int to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Write a type-annotated function to_str that takes a float n as
+    argument and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        Converts a float into a string
+    """
+    return str(n)
