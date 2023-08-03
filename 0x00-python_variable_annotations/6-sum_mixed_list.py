acdadc7916154604bc78e5a44acda91b1306b431
itsmraga-hub <ragawilliam570@gmail.com>
2023-08-03 17:02:28 +0300

    Task 6: add a mixed list of ints and floats

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Write a type-annotated function sum_mixed_list which takes a list
+    mxd_lst of integers and floats and returns their sum as a float.
+"""
+import functools
+
+
+def sum_mixed_list(mxd_lst: list(int|float)): -> float:
+    """
+        A function that adds both ints and floats
+        and returns a float
+    """
+    return functools.reduce(lambda a, b: a + b, mxd_lst)
