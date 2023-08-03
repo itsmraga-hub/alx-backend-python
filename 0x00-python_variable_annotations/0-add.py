121afce9bb782f7f668db5c7e73fad2c6d390197
itsmraga-hub <ragawilliam570@gmail.com>
2023-08-03 17:00:29 +0300

    Task 0: add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Write a type-annotated function add that takes a float a
+    and a float b as arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        add function that returns total of two numbers
+    """
+    return a + b
