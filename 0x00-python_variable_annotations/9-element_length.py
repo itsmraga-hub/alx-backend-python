b280822ca02888819e3d45f8cc78571b3bc9aef8
itsmraga-hub <ragawilliam570@gmail.com>
2023-08-03 20:49:09 +0300

    Task  9 Annotate an iterable object

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Annotate the below function’s parameters and return values with
+    the appropriate types
+"""
+from typing import Iterable, Sequence, Tuple, List
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+        Annotate the below function’s parameters and return
+        values with the appropriate types
+    """
+    return [(i, len(i)) for i in lst]
