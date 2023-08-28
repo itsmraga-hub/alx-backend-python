def368f0136974a3e9393f274108c55f0e6e839e
itsmraga-hub <ragawilliam570@gmail.com>
2023-08-28 17:59:18 +0300

    Task 0

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Implement the TestAccessNestedMap.test_access_nested_map method to test
+    that the method returns what it is supposed to.
+"""
+import unittest
+
+
+
+
+class TestAccessMestedMap(unittest.TestCase):
+    """
+        Inherits from unittest.TestCase
+    """
+
